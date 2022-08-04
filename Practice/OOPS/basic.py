e92ba7f453afe4f33566b703dda9b007adedb849
Deep <48485675+Just2Deep@users.noreply.github.com>
2022-08-04 21:43:21 +0530

    Update basic.py

--- Practice/OOPS/basic.py
+++ Practice/OOPS/basic.py
@@ -31,7 +31,7 @@ class Employee():
         first, last, pay = emp_str.split('-')
         return cls(first, last, pay)
 
-    @staticmethod                                                # does not take any self/class instances
+    @staticmethod                                                # does not take any self/class parameters
     def is_workday(day):
         if day.weekday() == 5 or day.weekday() == 6:
             return False
