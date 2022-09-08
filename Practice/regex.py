a44f7e9f8d65d58d5950ee481a858f6195dd23c7
Just2Deep <deepakrajsrs@gmail.com>
2022-09-08 08:30:59 +0530

    urls matching

--- Practice/regex.py
+++ Practice/regex.py
@@ -3,13 +3,33 @@
 import re
 
 emails = '''
-CoreyMSchafer@gmail.com
-corey.schafer@university.edu
-corey-321-schafer@my-work.net
-'''
+            CoreyMSchafer@gmail.com
+            corey.schafer@university.edu
+            corey-321-schafer@my-work.net
+         '''
 pattern = re.compile(r'[a-zA-Z0-9-._]+@[a-zA-Z0-9-]+\.[a-zA-z]+')
 
 matches = pattern.finditer(emails)
 
-for match in matches:
-    print(match)
+# for match in matches:
+#     print(match)
+
+
+urls = '''
+        https://www.google.com
+        http://coreyms.com
+        https://youtube.com
+        https://www.nasa.gov
+       '''
+
+pattern = re.compile(r'https?://(www\.)?(\w+)(\.\w+)')
+
+subbed_urls = pattern.sub(r'\2\3', urls)
+
+print(subbed_urls)
+
+# matches = pattern.finditer(urls)
+
+# for match in matches:
+#     # print(match.group(0))
+#     print(match.group(2) + match.group(3))
