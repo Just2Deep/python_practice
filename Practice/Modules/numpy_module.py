27ecb16ac226c157828ffb00e6baf3d4ff10f39e
Just2Deep <deepakrajsrs@gmail.com>
2022-09-19 20:20:30 +0530

    numpy modifications

--- Practice/Modules/numpy_module.py
+++ Practice/Modules/numpy_module.py
@@ -174,3 +174,144 @@ print(a[b])
 # finding like a even numbers
 even = np.argwhere(a % 2 == 0).flatten()
 print(a[even])
+
+# arange in numpy
+
+a = np.arange(1, 7)
+print(a)
+print(a.shape)
+b = a.reshape(2, 3)
+print(b)
+
+c = a[np.newaxis, :]
+print(c)
+print(c.shape)
+
+d = a[:, np.newaxis]
+print(d)
+print(d.shape)
+
+
+# concatenation
+
+a = np.array([[1, 2], [3, 4]])
+print(a)
+b = np.array([[5, 6]])
+
+c = np.concatenate((a, b.T), axis=1)
+print(c)
+
+# hstack & vstack
+
+a = np.array([1, 2, 3, 4])
+b = np.array([5, 6, 7, 8])
+
+c = np.vstack((a, b))
+d = np.hstack((a, b))
+print(c, d)
+
+# array and vector
+
+x = np.array([[1, 2, 3], [4, 5, 6], [1, 2, 3], [4, 5, 6]])
+a = np.array([1, 0, 1])
+
+print(x)
+print(x+a)
+
+# datascience important things
+
+a = np.array([[7, 8, 9, 10, 11, 12, 13], [17, 181, 19, 20, 21, 22, 23]])
+print(a)
+print(a.sum(axis=1))
+print(a.sum(axis=0))
+print(a.sum(axis=None))
+
+
+# setting data types
+
+x = np.array([1.0, 2.0], dtype=np.int32)
+print(x)
+print(x.dtype)
+
+# copying arrays
+
+a = np.array([1, 2, 3])
+b = a.copy()
+b[0] = 42
+print(a, b)
+
+# generating arrays
+a = np.zeros((2, 3))
+print(a)
+
+b = np.ones((2, 3))
+print(b)
+
+a = np.full((2, 3), 5.0)
+print(a)
+
+a = np.eye(3)
+print(a)
+
+b = np.arange(20)
+print(b)
+
+c = np.linspace(0, 10, 5)
+print(c)
+
+# random numbers
+
+a = np.random.random((3, 2))  # (0-1)
+print(a)
+
+a = np.random.random(1000)  # normal/gaussian
+print(a.mean(), a.var())
+
+a = np.random.randint(1, 10, size=(3, 3))
+print(a)
+
+a = np.random.choice([-8, -2, -5, 1, 5], size=10)
+print(a)
+
+
+# linalg modules
+# eigen values
+
+a = np.array([[1, 2], [3, 4]])
+eigenvalues, eigenvectors = np.linalg.eig(a)
+
+print(eigenvalues)
+print(eigenvectors)  # column vector
+
+b = eigenvectors[:, 0] * eigenvalues[0]
+print(b)
+
+c = a @ eigenvectors[:, 0]
+print(c)
+
+print(np.allclose(b, c))
+
+
+# solving linear algebra
+# x1 + x2 = 2200
+# 1.5x1 + 4x2 = 5050
+
+# A = ([1 1], [1.5 4]) b = ([2200 5050])
+# Ax = b == x = A^-1 b
+
+A = np.array([[1, 1], [1.5, 4.0]])
+b = np.array([2200, 5050])
+
+x = np.linalg.inv(A).dot(b)
+print(x)
+
+
+# alternate method
+x = np.linalg.solve(A, b)
+print(x)
+
+# loading data from csv
+#np.loadtxt, np.genfromtxt
+
+#data = np.loadtxt('filename.csv', delimiter=',', dtype=np.float32)
+#data = np.genfromtxt('filename.csv', delimiter=',', dtype=np.float32)
